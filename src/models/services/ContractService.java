0fdc1770a3a3af4b3c1a3299f8e25cb9b338f6e3
Antonio Pires <tonan@globo.com>
2020-10-23 14:57:07 -0300

    Cabeça bugada 2

--- src/models/services/ContractService.java
+++ src/models/services/ContractService.java
@@ -1,10 +1,19 @@
 package models.services;
 
+import java.text.SimpleDateFormat;
+
 import models.entities.Contract;
 
 public class ContractService {
+	
+	private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public void processContract(Contract contract,Integer months) {
+		
+		for (int i=0;i<months;i++) {
+			
+			
+		}
 		
 	}
 }
